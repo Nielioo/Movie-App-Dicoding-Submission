368a0bcd6ffe729d42275a918bd4cdf4c7e8ea35
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-09-19 09:16:05 +0700

    resolved overflowed view

--- movie_app/lib/main.dart
+++ movie_app/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:movie_app/views/pages.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
